4081777ecd340d72131037b4c4525ff284664d50
chikopaidze <nchikopaidze@gmail.com>
2022-03-19 12:13:58 +0400

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("wtf")
+board = [['x', '', ''], ['', '', ''], ['', '', '']]
+
+
+def print_board():
+    for i in range(3):
+        for j in range(3):
+            print("|", board[i][j], "|", end="")
+        print("\n", "__________")
+
+
+print_board()
